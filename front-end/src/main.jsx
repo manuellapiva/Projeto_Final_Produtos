3782fecb622a46c4f4885d66f0cb26b6c913651c
manuellapiva <manuellabpiva@gmail.com>
2026-04-28 09:54:42 -0300

    alterando a main e adicionando conteudo no produtos.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Produtos from '../pages/produtos.jsx'
+import Equipe from '../pages/equipe.jsx'
+import Home from '../pages/home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Produtos/>
+    <Equipe/>
+    <Home/>
   </StrictMode>,
 )
