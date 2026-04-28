7fe2a71512d57d02a78184ce023c007b288d9151
ayla-vilela <ayla.vilela51@gmail.com>
2026-04-28 13:41:14 -0300

    comits finais da home, com o css arrumado, e o jsx com a descrição pedida

--- front-end/pages/home.jsx
+++ front-end/pages/home.jsx
@@ -1,16 +1,21 @@
-function Home() { 
+import style from "./Home.module.css";
+
+function Home() {
   return (
-    <>
-      <ul className="descricao">
-        <li className="objtop">Tecnologias Utilizadas</li>
-        <li className="obj">⚛️ React</li>
-        <li className="obj">⚡ Vite</li>
-        <li className="obj">🧠 JavaScript</li>
-        <li className="obj">🔀 React Router</li>
-        <li className="obj">🌐 Fetch</li>
-        <li className="obj">🎨 CSS (livre)</li>
-      </ul>
-    </>
+    <div className={style.container}>
+      <div className={style.descricao}>
+        <li className={style.objtop}>Tecnologias Utilizadas</li>
+
+        <li className={style.obj}>⚛️ React</li>
+        <li className={style.obj}>⚡ Vite</li>
+        <li className={style.obj}>🧠 JavaScript</li>
+        <li className={style.obj}>🔀 React Router</li>
+        <li className={style.obj}>🌐 Fetch</li>
+        <li className={style.obj}>🎨 CSS (livre)</li>
+      </div>
+      <div className={style.descricao}><h3 className={style.objtop}>Quem somos ?</h3><p>Este sistema foi desenvolvido para gerenciar produtos de forma simples e eficiente. A aplicação permite cadastrar, visualizar, editar e excluir produtos, integrando um front-end moderno com um backend funcional. <br /><br /> O projeto utiliza  React, Vite e JavaScript, garantindo uma interface rápida, dinâmica e organizada. A comunicação com a API é feita através de requisições HTTP, proporcionando uma experiência completa de CRUD em tempo real. <br /><br />Além disso, o sistema foi estruturado seguindo boas práticas de desenvolvimento, com organização de código e separação entre frontend e backend.</p></div>
+    </div>
   );
 }
+
 export default Home;
